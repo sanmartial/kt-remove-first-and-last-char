3facd445ceaf9c5f9ade038b9c1d0f82f65da837
Roman Globa <globaroman@gmail.com>
2024-05-30 20:50:45 +0300

    The solution of the task

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,6 +1,11 @@
 package mate.academy
 
 fun removeChars(str: String): String {
-    // Implement this function
-    return ""
+    if (str.length <= 2) {
+        return ""
+    } else {
+        return str.drop(1).dropLast(1)
+
+    }
+
 }
